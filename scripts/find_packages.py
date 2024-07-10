4fa9a733330ec68a81c3f861adf2fc79f372c240
Appie Verschoor <appie@cooljapan.nl>
2024-07-10 14:55:39 +0200

    keep failing

--- scripts/find_packages.py
+++ scripts/find_packages.py
@@ -2,6 +2,7 @@ import sys
 import glob
 import os.path
 import yaml
+import subprocess
 
 
 if len(sys.argv) != 2:
@@ -13,11 +14,17 @@ if os.path.isfile("_tax/testconfig.yaml"):
         try:
             repositories = yaml.safe_load(f)
             packages = []
+            subprocess.run(["mkdir", "_tmp"])
             for repo in repositories["repositories"]:
                 branch = repo.get("branch")
                 name = repo.get("name")
                 package = repo.get("package")
+                git_cmd = f"cd _tmp; git clone {name} --branche {branch}; cd .."
+                subprocess.run(git_cmd, shell=True)
+                package_cmd = f"cd _tmp; zip -r {package} {name}{package}; cd .."
+                subprocess.run(package_cmd, shell=True)
                 packages.append(f"public/taxonomies/{branch}/{package}.zip")
+            print(subprocess.run("ls -l _tmp"))
         except yaml.YAMLError as exc:
             print(exc)
             exit(1)
