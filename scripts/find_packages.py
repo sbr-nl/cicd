49a35889c9c6e1f4192c9fca7cecfb22a44f82d0
Appie Verschoor <appie@cooljapan.nl>
2024-07-10 20:34:44 +0200

    enfin, in de root iets aanmaken mag dus niet in een runner. je mag ook niks meer (balt vuist richting de schuldigen)

--- scripts/find_packages.py
+++ scripts/find_packages.py
@@ -15,21 +15,21 @@ if os.path.isfile("_tax/testconfig.yaml"):
         try:
             repositories = yaml.safe_load(f)
             packages = []
-            subprocess.run(["mkdir", "/tst"])
+            subprocess.run(["mkdir", "tst"])
             subprocess.run("pwd", shell=True)
             for repo in repositories["repositories"]:
                 branch = repo.get("branch")
                 name = repo.get("name")
                 package = repo.get("package")
                 git_cmd = f"PWD=`pwd` \
-                cd /tst && \
+                cd tst && \
                 git clone https://github.com/{owner}/{name} --branch {branch} && \
                 cd {name} \
                 zip -qr /tst/{package} {package} && \
-                cd $PWD"
+                cd $PWD "
                 subprocess.run(git_cmd, shell=True)
-                packages.append(f"/tst/{package}.zip")
-            print(subprocess.run("ls -l /tst", shell=True))
+                packages.append(f"tst/{package}.zip")
+            print(subprocess.run("ls -l tst", shell=True))
         except yaml.YAMLError as exc:
             print(exc)
             exit(1)
