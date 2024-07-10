4980f8199a8447268e47fe0f2b7f08537f6650a4
Appie Verschoor <appie@cooljapan.nl>
2024-07-10 14:08:29 +0200

    lees testconfig.yaml

--- scripts/find_packages.py
+++ scripts/find_packages.py
@@ -13,10 +13,15 @@ if os.path.isfile("_tax/testconfig.yaml"):
     with open("_tax/testconfig.yaml") as f:
         try:
             repositories = yaml.safe_load(f)
+            packages = []
+            for repo in repositories["repositories"]:
+                branch = repo.get("branch")
+                name = repo.get("name")
+                package = repo.get("package")
+                packages.append(f"public/taxonomies/{branch}/{package}")
         except yaml.YAMLError as exc:
             print(exc)
             exit(1)
-    print(repositories)
 else:
     pattern = "%s/*zip" % sys.argv[1]
     packages = glob.glob(pattern)
