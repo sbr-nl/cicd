c81399c92657b7b9e24459f37e6caccd07cad4c4
Appie Verschoor <appie@cooljapan.nl>
2024-07-31 14:55:25 +0200

    why tell me why

--- scripts/find_packages.py
+++ scripts/find_packages.py
@@ -22,7 +22,7 @@ if os.path.isfile("_tax/testconfig.yaml"):
                 package = repo.get("package")
                 git_cmd = ["git", "clone",  f"https://github.com/{owner}/{name}.git", "--branch", branch]
                 # package_cmd = ["cd",  name, " && ", "zip",  "-r",  f"../{package}",  package, " && ", "cd",  ".."]
-                package_cmd = ["zip", "-r", f"{name}/{package}", package]
+                package_cmd = ["cd", name, "&&", "zip", "-r", f"{package}", package]
                 print(" ".join(git_cmd))
                 subprocess.run(git_cmd)
                 subprocess.run(package_cmd)
