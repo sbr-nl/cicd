6bc5a834bb1395160072875ddccde396ac6f1b00
Appie Verschoor <appie@cooljapan.nl>
2024-08-20 22:29:44 +0200

    order, order!

--- scripts/find_packages.py
+++ scripts/find_packages.py
@@ -21,10 +21,11 @@ if os.path.isfile("_tax/testconfig.yaml"):
                 name = repo.get("name")
                 package = repo.get("package")
                 git_cmd = ["git", "clone",  f"https://github.com/{owner}/{name}.git", "--branch", branch]
+                subprocess.run(git_cmd)
+
                 os.chdir(name)
                 # package_cmd = ["cd",  name, " && ", "zip",  "-r",  f"../{package}",  package, " && ", "cd",  ".."]
                 package_cmd = ["zip", "-r", f"../_tax/{package}", f"{package}"]
-                subprocess.run(git_cmd)
                 outp = subprocess.run(package_cmd, shell=True, capture_output=True)
                 print(outp)
                 os.chdir("..")
